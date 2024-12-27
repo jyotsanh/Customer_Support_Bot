b4adbc321397260bdc4c0c14d971a163ea475a72
jyotsanh <hamaljyotsan@gmail.com>
2024-12-27 09:17:30 +0545

    new system prompt for main agent to follwo the guideline

--- src/prompts/primary_assistant.py
+++ src/prompts/primary_assistant.py
@@ -2,12 +2,9 @@
 from config.settings import get_llm
 
 # tools
-from tools.math_tool import multiply, add, subtract
-from tools.hotel_info_tool import get_info_about_hotel_bomo
-from tools.book_info_tool import book_hotel, cancel_booking, update_hotel_info
+from tools.tour_booking_info import *
 
 # langchain
-from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_core.prompts import ChatPromptTemplate
 
 #libs
@@ -17,51 +14,63 @@ import os
 
 load_dotenv()
 
-TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
-
 
 
 part_2_tools = [
-        TavilySearchResults(max_results=1),
-        # get_info_about_hotel_bomo,
-        # book_hotel,
-        # cancel_booking,
-        # update_hotel_info
+        book_tour,
+        cancel_tour_booking,
+        reschedule_tour_time,
+        
+        get_today_date,
+        get_available_slots,
+        get_current_booking,
+        is_date_valid,
+        
+        get_steps_for_booking_tour,
+        get_steps_for_reschedule_tour,
+        get_steps_for_cancelling_tour
+    
     ]
 
 def create_primary_assistant_runnable():
-    primary_assistant_prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                "You are a Chatbot Assistant for Hotel Bomo. Answer any query related to Hotel Bomo using `get_info_about_hotel` tools. "
-                " Before booking a room , you need to check the customer if they are register or not via verification code. "
-                "Always refer to Hotel Bomo in the first person (e.g., 'we,' 'our') when discussing its offerings, services, and features. "
-                "Before booking room make sure to ask the required arguments for booking the room. "
-                "For example, say 'We offer a multi-cuisine dining experience' instead of 'They offer a multi-cuisine dining experience.' "
-                "Use the provided tools to assist the user's queries. "
-                "When searching, be persistent. Expand your query bounds if the first search returns no results. "
-                "If a search comes up empty, expand your search before giving up."
-            ),
-            ("placeholder", "{messages}"),
-        ]
-    ).partial(time=datetime.now)
+
+    """
+    Creates a primary assistant runnable with tools for booking, rescheduling, 
+    and canceling tours using LangChain's ChatPromptTemplate and integrated LLMs.
     
+    Returns:
+        AssistantRunnable: Configured runnable for handling tour management queries.
+    """
     test_primary_assistant_prompt = ChatPromptTemplate.from_messages(
         [
             (
                 "system",
-                "Answer any query related to Hotel Bomo [`location`,`description`,`rooms`,`dining`,`activities`,`contact_info`] using `get_info_about_hotel` tools."
-                "Before running the tools, repeat back the required arguments before invoking the tools. "
-                "You are a Customer Support Assistant for Hotel Bomo."
-                "Always refer to Hotel Bomo in the first person (e.g., 'we,' 'our') when discussing its offerings, services, and features. "
+                (
+                   """
+                   ## CRITICAL GUIDELINES YOU HAVE TO FOLLOW:
+                    You are a helpful customer support assistant for booking a tour.
+                    `DO NOT MENTION WHAT YOU ARE GOING TO DO, JUST ASK USER`
+                    before booking check if there customer has already booked , if booked then ask `do you want to reschedule` if not follow steps
+                    Don't ask Customer information in one go, ask for it step by step.
+                    You always have to follow `get_steps` tool which will provide you a proper steps for booking, rescheduling or canceling a tour.
+                    Be clear, concise, and use the tools to provide the required information for each step of the process.
+                    If additional details are needed or the user has questions, guide them accordingly.
+                    When searching, be persistent. Expand your query bounds if the first search returns no results.
+                    If a search comes up empty, try a broader search before giving up.
+                    \nPresent time: {time}.
+                    \nPresent Year: {year}
+                    """
+                ),
             ),
             ("placeholder", "{messages}"),
         ]
-    ).partial(time=datetime.now)
-    
+    ).partial(
+        time=datetime.now,
+        year=datetime.now().year
+    )
+    print(datetime.now().year)
     # llms available: openai, google, groq
-    llm = get_llm('google') #-> return llm based on param passed
+    llm = get_llm('openai') #-> return llm based on param passed
     # part_1_assistant_runnable = primary_assistant_prompt | llm.bind_tools(part_1_tools)
     part_2_assistant_runnable = test_primary_assistant_prompt | llm.bind_tools(part_2_tools)
     print("Returning the assistant runnable with tools")
